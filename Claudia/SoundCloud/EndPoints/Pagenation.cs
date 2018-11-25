9c23b0cb92b947e385932968c15dabc497e35c1d
Asterisk <kuro5858tube@gmail.com>
2018-11-26 00:58:40 +0900

    [wip] SoundCloud お気に入り曲取得のページネーション処理🐈🐈🐈

---
+++ Claudia/SoundCloud/EndPoints/Pagenation.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Claudia.SoundCloud.EndPoints
+{
+	public class Pagenation<T>
+	{
+		[JsonProperty("collection")]
+		public List<T> Collection { get; private set; }
+
+		[JsonProperty("next_href")]
+		public string NextHref { get; private set; }
+	}
+}
